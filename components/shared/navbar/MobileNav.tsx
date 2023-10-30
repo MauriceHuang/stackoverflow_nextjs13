fc8374bc7e0709a96c77b4ecb9233cebe883bb94
Maurice Huang <maurice.huang@goalsmapper.com>
2023-10-31 01:19:32 +0800

    #11 mobile navbar with sidelinks

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,19 +1,52 @@
+"use client";
 import React from "react";
 import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Image from "next/image";
 import Link from "next/link";
 import { SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 const NavContent = () => {
-  return <section className="flex flex-col h-full gap-6 pt-16"></section>;
+  const pathname = usePathname();
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname === item.route && item.route.length > 1) ||
+          pathname === item.route;
+        return (
+          <SheetClose asChild key={item.route}>
+            <Link
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              }
+               flex items-center justify-start gap-4 rounded-lg p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
 };
 const MobileNav = () => {
   return (
